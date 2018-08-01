2e2166e8f1079b9a0d19223b1cb1e222675d7930
jinseongjoo <qpyou1234@naver.com>
2018-08-01 23:10:48 +0900

    Getting the Weather 완료

--- weather/App.js
+++ weather/App.js
@@ -2,20 +2,34 @@ import React,{ Component } from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Weather from './Weather';
 
+const API_KEY=""
+
 export default class App extends Component {
   state = {
     isLoaded:false,
-    error:null
+    error:null,
+    temperature:null,
+    name:null
   };
   componentDidMount(){
     navigator.geolocation.getCurrentPosition(position => {
-      this.setState({
-        isLoaded:true
-      })
+      this._getWeather(position.coords.latitude,position.coords.longitude)
     },
     error => {
       this.setState({
         error:error
+      })
+    })
+  }
+  _getWeather=(lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+      this.setState({
+        temperature:json.main.temp,
+        name:json.weather[0].main,
+        isLoaded:true
       })
     })
   }
